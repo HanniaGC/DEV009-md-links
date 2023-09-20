68c3ba8afda3662a503edda4344370980c010665
HanniaGC <hannia_gc@outlook.com>
2023-09-19 21:51:03 -0600

    Test Hito 1

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,13 +1,38 @@
 const mdLinks = require('../index.js');
+const functions = require('../data.js')
 
 
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
   it('Deberia devolver una promesa', () => {
-    expect(mdLinks()).toBe(typeof Promise);
+    const result = mdLinks();
+    expect(result instanceof Promise).toBe(true);
 
   })
+});
+
+// Mock de las funciones 
+functions.isAbsolutePath = jest.fn().mockReturnValue(true);
+functions.pathExist = jest.fn().mockReturnValue(true);
+functions.isMarkdownFile = jest.fn().mockReturnValue(true);
+functions.readMarkdownFile = jest.fn().mockResolvedValue(`
+  [Enlace 1](https://www.ejemplo1.com)
+  [Enlace 2](https://www.ejemplo2.com)
+  [Enlace 3](https://www.ejemplo3.com)
+`);
+functions.findLinksInMarkdown = jest.fn().mockReturnValue([
+  { text: 'Enlace 1', href: 'https://www.ejemplo1.com' },
+  { text: 'Enlace 2', href: 'https://www.ejemplo2.com' },
+  { text: 'Enlace 3', href: 'https://www.ejemplo3.com' },
+]);
+
+describe('mdLinks', () => {
+  it('debería resolver un arreglo con 3 links para un archivo .md con 3 links', () => {
+    return mdLinks('miArchivo.md').then((result) => {
+      expect(result).toEqual([
+        { text: 'Enlace 1', href: 'https://www.ejemplo1.com' },
+        { text: 'Enlace 2', href: 'https://www.ejemplo2.com' },
+        { text: 'Enlace 3', href: 'https://www.ejemplo3.com' },
+      ]);
+    });
+  });
 });
